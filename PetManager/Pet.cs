71c754da906fa474272f0021ee980c9e9dfb6ff9
J Doe <jdoe@contonso.com>
2018-08-28 16:29:58 +0200

    implement Aufgabe Rechner B

--- PetManager/Pet.cs
+++ PetManager/Pet.cs
@@ -8,5 +8,33 @@ namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+        public int Age { get; set; }
+        public Pet() { }
+        public Pet(int day, int month, int year)
+        {
+            var date = new DateTime(year: year, month: month, day: day);
+            Birthday = date;
+            CheckYear(date);
+            Age = CalculateAge(date);
+        }
+
+        private void CheckYear(DateTime dt)
+        {
+            var yearNow = DateTime.Now.Year;
+            var petYear = Birthday.Year;
+            if (petYear > yearNow) throw new Exception("Entered year is in the future..");
+        }
+
+        private int CalculateAge(DateTime date)
+        {
+            var petyears = date.Year;
+            var now = DateTime.Now.Year;
+            return now - petyears;
+        }
     }
 }
